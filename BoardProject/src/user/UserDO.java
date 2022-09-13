085be01e16237d9f6abed2c4cbfdab3b308d2b0f
USER <USER@DESKTOP-PBISV4L>
2022-09-13 11:17:30 +0900

    UserDO 클래스에 getter, setter 메소드 추가

--- BoardProject/src/user/UserDO.java
+++ BoardProject/src/user/UserDO.java
@@ -6,4 +6,32 @@ public class UserDO {
 	private String password;	//패스워드
 	private String name;		//이름
 	private String role;		//역할
+	
+	//각 필드 하나당 getter, setter 메소드 구현
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
 }
