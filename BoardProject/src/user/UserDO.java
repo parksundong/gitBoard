dc5d09288671e1225035cd1eee51dd7be3c5cc7f
USER <USER@DESKTOP-PBISV4L>
2022-09-13 09:33:32 +0900

    UserDO 클래스에 필드 선언 추가 - 두번째 커밋

--- BoardProject/src/user/UserDO.java
+++ BoardProject/src/user/UserDO.java
@@ -1,5 +1,9 @@
 package user;
 
 public class UserDO {
-
+	//필드 선언(멤버변수)
+	private String id;			//userID
+	private String password;	//패스워드
+	private String name;		//이름
+	private String role;		//역할
 }
